964c1e22f030bafc7d03a97499c17117b9712a14
Frank Panzer <bremercommunity@googlemail.com>
2019-12-10 09:01:06 +0100

    Listen
    Weiß noch nicht wie ich diese einsetze

---
+++ listen.py
@@ -0,0 +1,26 @@
+# Listen
+# Fahrzeuge, Kunden, Lieferanten, Rechnungen,Zweigstellen. Konten, Mitarbeiter...
+
+# Fahrzeuge
+# Fahrzeugmarken Quelle: https://www.kfz-mag.de/marken-modelle
+Fahrzeugmarken=["Acura","Alfa Romeo","Aston Martin","Audi","Bently","BMW","BMW i","Bugatti","Buick","Byton","Cadillac","Chevrolet","Chrysler","Citroen","Dacia","Daihatsu","Dodge","DS Automobiles","Faraday Future","Ferrari","Fiat","Fisker","Ford","GMC","Holden","Honda","Hyundai","Infiniti","Isuzu","Jaguar","Jeep","Kia","Koenigsegg","Lada","Lamborghini","Lancia","Land Rover","Lexus","Ligier","Lincoln","Lotus","Mahindra","Maserati","Maybach","Mazda","Mercedes-Benz","Mini","Mitsubishi","Morgan","Nissan","Opel","Pagani","Peugeot","PG","Piaggio","Porsche","Renault","Rolls-Royce","Saab","Seat","Skoda","Smart","SsangYong","StreetScooter","Subaru","Suzuki","Tesla","Toyota","Triumph","TVR","Vauxhall","Volvo","VW","Wiesmann"]
+
+# Kunden
+Kunden=["kundennummer","Vorname","Nachname","strasse","hausnummer","plz","ort","land"]
+
+# Lieferanten Quelle: https://de.wikipedia.org/wiki/Liste_der_gr%C3%B6%C3%9Ften_Automobilzulieferer
+Lieferanten=["Bosch","Continental","Denso","Magna","ZF Friedrichshafen","Aisin","Hyundai Mobis","Bridgestone-Firestone","Michelin","Valeo","Lear","Faurecia","Cummins","Adient","Goodyear","Sumitomo Electric","Yazaki","Mahle","Aptiv","Weichai Power","Panasonic","Toyota Boshoku","Schaeffler","Tenneco","BorgWarner","Gestamp","ThyssenKrupp Automotive","Yanfeng Automotive Interiors","Hitachi","Calsonic Kansei","JTEKT","Autoliv","GKN","Beijing Hainachuan Automotive Parts","Plastic Omnium","Johnson Controls","TE Connectivity","Joyson Electronics","Flex-N-Gate","Dana","Hella GmbH & Co.KGaA","Federal-Mogul","Brose","Toyoda Gosei","Koito Manufacturing","American Axle","Benteler","Samvardhana Motherson","Sumitomo Rubber Industries","NSK Group","Mitsubishi Electric","Pirelli","Harman","Grupo Antolin","Magneti Marelli","NTN","Hyundai WIA","Hankook Tire","Eberspächer","Alps Electric","Delphi Technologies","Dräxlmaier","Freudenberg","Hanon Systems","Nemak","Leoni","Tokai Rika","Mando","IAC","Meritor","Contemporary Amperex Technology CATL","Linamar","CITIC Dicastal","Yokohama Rubber","NHK Spring","Mann+Hummel","TI Automotive","NXP Semiconductors","Webasto","Nexteer Automotive","Infineon","Saint-Gobain","Wabco","Eaton","TS Tech","Stanley Electric","Futaba Industrial","Sumitomo Riko","Cooper Standard","Knorr-Bremse","Renesas","Asahi Glass","Toyo Tire & Rubber","Mitsuba Corp.","CIE Automotive","LG Electronics","Hutchinson","Garrett Motion Inc.","Rheinmetall Automotive","Illinois Tool Works"]
+
+# Rechnungen
+# Rechnungen nach Datum-Kundennummer-Rechnungsnummer
+# Beispiel für einen Datensatz 09122019-444422-000001
+Rechnungen=["Datum","Kundennummer","Rechnungsnummer","Rechnungssumme"]
+
+# Zweigstellen
+Zweigstellen=["Bremen City","Bremen Nord"]
+
+# Konten
+Konten=["Kundennummer","IBAN","BIC"]
+
+# Mitarbeiter
+Mitarbeiter=["MitarbeiterNr","Zweigstelle","Vorname","Nachname","strasse","hausnummer","plz","ort","land"]
