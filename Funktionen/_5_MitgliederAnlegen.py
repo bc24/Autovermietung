a7d70499bf6169b4127be49cef857400cd48d566
Frank Panzer <bremercommunity@googlemail.com>
2019-12-17 21:33:04 +0100

    Kurze Überarbeitung
    Kurze Überarbeitung an Mitglieder anlegen

--- Funktionen/_5_MitgliederAnlegen.py
+++ Funktionen/_5_MitgliederAnlegen.py
@@ -17,31 +17,39 @@ Zufall Datensaätze                  -   https://mockaroo.com/
 # Datenbanken Verbindung
 from mysql.connector import cursor
 
-def MitgliederAnlegen(conn, cursor):
-    cursor.execute(INSERT INTO mitarbeiter (vorname,nachname,strasse,hausnummer,plz_id,telefonnr) VALUES (%s,%s,%s,%s,%s,%s))
-# ID für Mitglieder wird gehollt
-    res = cursor.execute("INSERT INTO mitglieder (mitglieder_id) VALUES (%s,%s,%s,%s,%s,%s)", (MitgliederAnlegen,))
-    print(res.lastinsertid)
-    mgid = res.lastinsertid
-
 # Abfragen um Mitarbeiter anzulegen
+def MitgliederAnlegen(conn, cursor):
+
     Mitglied_Vorname = input("Bitte geben Sie eine Vorname ein, die Sie hinzufügen wollen: ")
+    liste_mitarbeiter=[]
+    liste_mitarbeiter.insert(0,Mitglied_Vorname)
+
     Mitglied_Nachname = input("Bitte geben Sie eine Nachname ein, die Sie hinzufügen wollen: ")
+    liste_mitarbeiter.insert(1,Mitglied_Nachname)
+
     Mitglied_Strasse = input("Bitte geben Sie eine Straße ein, die Sie hinzufügen wollen: ")
+    liste_mitarbeiter.insert(2,Mitglied_Strasse)
+
     Mitglied_Hausnummer = input("Bitte geben Sie eine Hausnummer ein, die Sie hinzufügen wollen: ")
+    liste_mitarbeiter.insert(3,Mitglied_Hausnummer)
+
     Mitglied_Telefonnummer = input("Bitte geben Sie eien Telefonnummer ein, die Sie hinzufügen wollen: ")
+    liste_mitarbeiter.insert(4,Mitglied_Telefonnummer)
+
     Mitglied_PLZ = input("Bitte geben Sie die PLZ ein, die Sie hinzufügen wollen: ")
+    liste_plz=[]
+    liste_plz.insert(0,Mitglied_PLZ)
+    liste_vergleich=[]
+    liste_vergleich.insert(0,Mitglied_PLZ)
 
-# Den Cursor holen und in einer Tupel übergeben
-    cursor = cursor.cursor()
+    Mitglied_Wohnort = input("Bitte geben Sie eien Wohnort ein, die Sie hinzufügen wollen: ")
+    Mitglied_PLZ.insert(1,Mitglied_Wohnort)
 
-    cursor.execute("UPDATE mitglieder SET plz_id=(SELECT plz_id FROM plz_id WHERE plz=? limit 1) WHERE mitglieder_id = ?", (Mitglied_PLZ, mgid))
-    cursor.execute("INSERT INTO mitglieder vorname=? WHERE mitglieder_id = ? ", (Mitglied_Vorname, mgid))
-    cursor.execute("UPDATE mitglieder SET nachname=? WHERE vorname = ? ", (Mitglied_Nachname, mgid))
-    cursor.execute("UPDATE mitglieder SET strasse=? WHERE nachname = ? ", (Mitglied_Strasse, mgid))
-    cursor.execute("UPDATE mitglieder SET hausnummer=? WHERE strasse = ? ", (Mitglied_Hausnummer, mgid))
-    cursor.execute("UPDATE mitglieder SET telefonnr=? WHERE hausnummer = ? ", (Mitglied_Telefonnummer, mgid))
+    liste_mitarbeiter.insert(liste_vergleich[0])
+    tuple_mitarbeiter = tuple(liste_mitarbeiter)
+    tuple_mitarbeiter_plz = tuple(liste_plz)
 
-
-    result = cursor.fetchall()
-    cursor.commit()
+    cursor.execute(INSERT INTO plz_id(plz,ort) VALUES (?,?),(tuple_mitarbeiter_plz))
+    cursor.execute(INSERT INTO mitarbeiter(vorname,nachname,strasse,hausnummer,telefonnr,plz_id) VALUES (?,?,?,?,?, (SELECT plz_id FROM plz_id WHERE plz = ? limit 1)),tuple_mitarbeiter)
+    DB_CBM.commit(conn, cursor)
+    hmenu()
